c04c234f9ba8eca33d747c89c5dd5910f443aa03
AlexandrMarchevskiy <alexmarchevski@gmail.com>
2024-10-31 01:54:17 +0300

    Add Allure reporting

--- test_apod.py
+++ test_apod.py
@@ -1,26 +1,31 @@
+import allure
 import datetime
 import os
 
 import pytest
-import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
+@allure.epic("NASA API Tests")
+@allure.feature("APOD")
 class TestAPOD:
     NASA_API_KEY = os.getenv("NASA_API_KEY")
     url = "https://api.nasa.gov/planetary/apod"
 
+    @allure.story("Basic API test")
     @pytest.mark.parametrize("field", ["title", "date", "explanation", "url"])
     def test_apod_basic(self, field, make_request):
         data = make_request()
         assert field in data, f'{field} field is missing in response'
 
+    @allure.story("Date field test")
     def test_apod_date_field(self, make_request):
         data = make_request()
         expected_date = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d')
         assert data["date"] == expected_date
 
+    @allure.story("Past date test")
     def test_apod_past_date(self, make_request):
         past_date = (datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=365)).strftime('%Y-%m-%d')
         data = make_request(params={"date": past_date})
